2980e0fb1628cc2b6871fc8c8ab5ce7263b70465
Romain Giot <romain.giot@u-bordeaux.fr>
2016-10-27 16:28:45 +0200

    Add a script to allow dsk  hfe conversion on the host

---
+++ dsk2hfe.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+if ! test $# -eq 1
+then
+	echo "Usage:" >&2
+	echo "$0 FNAME.dsk" >&2
+	exit -1
+fi
+
+
+input="$1"
+if ! test -f "$input"
+then
+	echo "$input does not exist" >&2
+	exit -1
+fi
+
+dockerdir=/tmp
+hostdir=$(realpath $(dirname $input))
+
+
+input=$dockerdir/$(basename "$input")
+output=$(dirname "$input")/$(basename "$input" .dsk).hfe
+
+echo Convert $input to $output
+
+docker run --rm=true \
+	-v $hostdir:$dockerdir \
+	-t cpcsdk/crossdev \
+	hxcfe "-finput:$input" "-foutput:$output" -conv:HXC_HFE
+
+
