b4a66fca632d2f007e596f0df183946a1afebc11
Romain Giot <romain.giot@u-bordeaux.fr>
2017-03-03 22:45:59 +0100

    Modify aft wrapper in order to allow to change the input port

--- wrappers/aft.sh
+++ wrappers/aft.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+
+# Usage:
+#    AFTDEVICE=/dev/ttyUSB1 aft.sh -p /dev/ttyUSB1
+#    aft.sh
+
 hostdir=$(pwd)
 dockerdir=/aftdir
+
+
+AFTDEVICE=${AFTDEVICE:-/dev/ttyUSB0}
 
 ls *.sna *.dsk
 docker run --rm=true \
 	--env="LD_LIBRARY_PATH=/usr/local/lib" \
 	-v $hostdir:$dockerdir \
-	--device /dev/ttyUSB0 \
+	--device $AFTDEVICE \
 	--workdir "$dockerdir" \
 	-t cpcsdk/crossdev \
 	/usr/local/bin/aft-minibooster $*
