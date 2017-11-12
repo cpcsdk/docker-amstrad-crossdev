f3fc0dd2b1758c2cc6aa7428fd1e969f57f3b3c4
Romain Giot <romain.giot@u-bordeaux.fr>
2017-11-12 12:11:10 +0100

    Add grafx2 wrapper script

---
+++ wrappers/grafx2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# Start grafx2. The current folder is used as pictures folder
+
+echo "Workdirectory is /pictures"
+
+docker run -e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix   \
+	-v /dev/snd:/dev/snd \
+	-v /dev/shm:/dev/shm \
+	-v /etc/machine-id:/etc/machine-id \
+	-v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+	-v /var/lib/dbus:/var/lib/dbus \
+	-v ~/.pulse:/home/arnold/.pulse \
+	-v $(pwd):/pictures \
+	--privileged \
+	--rm=true \
+	-i -t cpcsdk/crossdev \
+	bash -c 'cd /pictures && grafx2'
+
+
+
+
